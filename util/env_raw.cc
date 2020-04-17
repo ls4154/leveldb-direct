47421b9e01a273f71302ac252d596509f85abe7f
Youngjae Lee <ls4154.lee@gmail.com>
2020-04-17 19:30:41 +0900

    spdk test

--- util/env_raw.cc
+++ util/env_raw.cc
@@ -39,7 +39,111 @@
 #include "util/env_posix_test_helper.h"
 #include "util/raw_logger.h"
 
+extern "C" {
+#include "spdk/stdinc.h"
+#include "spdk/ioat.h"
+#include "spdk/nvme.h"
+#include "spdk/string.h"
+#include "spdk/env.h"
+}
+
 namespace leveldb {
+
+struct ctrlr_entry {
+  struct spdk_nvme_ctrlr* ctrlr;
+  struct ctrlr_entry* next;
+  char name[1024];
+};
+
+struct ns_entry {
+  struct spdk_nvme_ctrlr* ctrlr;
+  struct spdk_nvme_ns* ns;
+  struct ns_entry* next;
+  struct spdk_nvme_qpair* qpair;
+};
+
+struct ctrlr_entry* g_controllers = NULL;
+struct ns_entry* g_namespaces = NULL;
+
+bool g_vmd = false;
+
+bool probe_cb(void* cb_ctx, const struct spdk_nvme_transport_id* trid,
+              struct spdk_nvme_ctrlr_opts* opts)
+{
+  printf("Attaching to %s\n", trid->traddr);
+  return true;
+}
+
+void register_ns(struct spdk_nvme_ctrlr* ctrlr, struct spdk_nvme_ns* ns)
+{
+  struct ns_entry *entry;
+
+  if (!spdk_nvme_ns_is_active(ns))
+    return;
+
+  entry = static_cast<ctrlr_entry*>(malloc(sizeof(struct ns_entry)));
+  if (entry == NULL) {
+    perror("ns_entry malloc");
+    exit(1);
+  }
+
+  entry->ctrlr = ctrlr;
+  entry->ns = ns;
+  entry->next = g_namespaces;
+  g_namespaces = entry;
+}
+
+
+void attach_cb(void* cb_ctx, const struct spdk_nvme_transport_id* trid,
+               struct spdk_nvme_ctrlr* ctrlr, const struct spdk_nvme_ctrlr_opts* opts)
+{
+  int nsid, num_ns;
+  struct ctrlr_entry* entry;
+  struct spdk_nvme_ns* ns;
+  const struct spdk_nvme_ctrlr_data* cdata;
+
+  entry = static_cast<ctrlr_entry*>(malloc(sizeof(struct ctrlr_entry)));
+  if (entry == NULL) {
+    perror("ctrlr_entry malloc");
+    exit(1);
+  }
+
+  printf("Attachedto %s\n", trid->traddr);
+  cdata = spdk_nvme_ctrlr_get_data(ctrlr);
+
+  snprintf(entry->name, sizeof(entry->name), "%-20.20s (%-20.20s)", cdata->mn, cdata->sn);
+  entry->ctrlr = ctrlr;
+  entry->next = g_controllers;
+  g_controllers = entry;
+
+  num_ns = spdk_nvme_ctrlr_get_num_ns(ctrlr);
+  printf("Using controller %s with %d namespaces.\n", entry->name, num_ns);
+  for (nsid = 1; nsid <= num_ns; nsid++) {
+    ns = spdk_nvme_ctrlr_get_ns(ctrlr, nsid);
+    if (ns == NULL)
+      continue;
+    register_ns(ctrlr, ns);
+  }
+}
+
+void cleanup(void)
+{
+  struct ns_entry *ns_entry = g_namespaces;
+  struct ctrlr_entry *ctrlr_entry = g_controllers;
+
+  while (ns_entry) {
+    struct ns_entry *next = ns_entry->next;
+    free(ns_entry);
+    ns_entry = next;
+  }
+
+  while (ctrlr_entry) {
+    struct ctrlr_entry *next = ctrlr_entry->next;
+    spdk_nvme_detach(ctrlr_entry->ctrlr);
+    free(ctrlr_entry);
+    ctrlr_entry = next;
+  }
+}
 
 namespace {
 
@@ -556,6 +660,33 @@ class PosixEnv : public Env {
 PosixEnv::PosixEnv()
     : background_work_cv_(&background_work_mutex_),
       started_background_thread_(false) {
+
+  int rc;
+  struct spdk_env_opts opts;
+
+  spdk_env_opts_init(&opts);
+  opts.name = "leveldb";
+  opts.shm_id = 0;
+  if (spdk_env_init(&opts) < 0) {
+    fprintf(stderr, "spdk_env_init failed\n");
+    exit(1);
+  }
+
+  rc = spdk_nvme_probe(NULL, NULL, probe_cb, attach_cb, NULL);
+  if (rc != 0) {
+    fprintf(stderr, "spdk_nvme_probe failed\n");
+    exit(1);
+  }
+
+  if (g_controllers == NULL) {
+    fprintf(stderr, "no NVMe contollers found\n");
+    cleanup();
+    exit(1);
+  }
+
+
+
+
   bool real_dev = false;
   dev_size_ = FS_SIZE;
   dev_fd_ = open("/dev/sda1", O_RDWR | O_CREAT, 0644);
