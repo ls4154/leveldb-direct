0986c1863a1c9fcd1adb7d679ea574cca4d254ea
Youngjae Lee <ls4154.lee@gmail.com>
2020-06-13 17:10:59 +0900

    remove unused

--- util/env_posix.cc
+++ util/env_posix.cc
@@ -41,15 +41,6 @@ namespace {
 #define MAX_OBJ_SIZE (4 * 1024 * 1024)
 int g_mem_fd;
 
-// Set by EnvPosixTestHelper::SetReadOnlyMMapLimit() and MaxOpenFiles().
-int g_open_read_only_file_limit = -1;
-
-// Up to 1000 mmap regions for 64-bit binaries; none for 32-bit.
-constexpr const int kDefaultMmapLimit = (sizeof(void*) >= 8) ? 1000 : 0;
-
-// Can be set using EnvPosixTestHelper::SetReadOnlyMMapLimit.
-int g_mmap_limit = kDefaultMmapLimit;
-
 constexpr const size_t kWritableFileBufferSize = 65536;
 
 Status PosixError(const std::string& context, int error_number) {
@@ -60,46 +51,6 @@ Status PosixError(const std::string& context, int error_number) {
   }
 }
 
-// Helper class to limit resource usage to avoid exhaustion.
-// Currently used to limit read-only file descriptors and mmap file usage
-// so that we do not run out of file descriptors or virtual memory, or run into
-// kernel performance problems for very large databases.
-class Limiter {
- public:
-  // Limit maximum number of resources to |max_acquires|.
-  Limiter(int max_acquires) : acquires_allowed_(max_acquires) {}
-
-  Limiter(const Limiter&) = delete;
-  Limiter operator=(const Limiter&) = delete;
-
-  // If another resource is available, acquire it and return true.
-  // Else return false.
-  bool Acquire() {
-    int old_acquires_allowed =
-        acquires_allowed_.fetch_sub(1, std::memory_order_relaxed);
-
-    if (old_acquires_allowed > 0) return true;
-
-    acquires_allowed_.fetch_add(1, std::memory_order_relaxed);
-    return false;
-  }
-
-  // Release a resource acquired by a previous call to Acquire() that returned
-  // true.
-  void Release() { acquires_allowed_.fetch_add(1, std::memory_order_relaxed); }
-
- private:
-  // The number of available resources.
-  //
-  // This is a counter and is not tied to the invariants of any other class, so
-  // it can be operated on safely using std::memory_order_relaxed.
-  std::atomic<int> acquires_allowed_;
-};
-
-// Implements sequential read access in a file using read().
-//
-// Instances of this class are thread-friendly but not thread-safe, as required
-// by the SequentialFile API.
 class PosixSequentialFile final : public SequentialFile {
  public:
   PosixSequentialFile(std::string filename, int fd)
@@ -135,92 +86,15 @@ class PosixSequentialFile final : public SequentialFile {
   const std::string filename_;
 };
 
-// Implements random read access in a file using pread().
-//
-// Instances of this class are thread-safe, as required by the RandomAccessFile
-// API. Instances are immutable and Read() only calls thread-safe library
-// functions.
-class PosixRandomAccessFile final : public RandomAccessFile {
- public:
-  // The new instance takes ownership of |fd|. |fd_limiter| must outlive this
-  // instance, and will be used to determine if .
-  PosixRandomAccessFile(std::string filename, int fd, Limiter* fd_limiter)
-      : has_permanent_fd_(fd_limiter->Acquire()),
-        fd_(has_permanent_fd_ ? fd : -1),
-        fd_limiter_(fd_limiter),
-        filename_(std::move(filename)) {
-    if (!has_permanent_fd_) {
-      assert(fd_ == -1);
-      ::close(fd);  // The file will be opened on every read.
-    }
-  }
-
-  ~PosixRandomAccessFile() override {
-    if (has_permanent_fd_) {
-      assert(fd_ != -1);
-      ::close(fd_);
-      fd_limiter_->Release();
-    }
-  }
-
-  Status Read(uint64_t offset, size_t n, Slice* result,
-              char* scratch) const override {
-    int fd = fd_;
-    if (!has_permanent_fd_) {
-      fd = ::open(filename_.c_str(), O_RDONLY);
-      if (fd < 0) {
-        return PosixError(filename_, errno);
-      }
-    }
-
-    assert(fd != -1);
-
-    Status status;
-    ssize_t read_size = ::pread(fd, scratch, n, static_cast<off_t>(offset));
-    *result = Slice(scratch, (read_size < 0) ? 0 : read_size);
-    if (read_size < 0) {
-      // An error: return a non-ok status.
-      status = PosixError(filename_, errno);
-    }
-    if (!has_permanent_fd_) {
-      // Close the temporary file descriptor opened earlier.
-      assert(fd != fd_);
-      ::close(fd);
-    }
-    return status;
-  }
-
- private:
-  const bool has_permanent_fd_;  // If false, the file is opened on every read.
-  const int fd_;                 // -1 if has_permanent_fd_ is false.
-  Limiter* const fd_limiter_;
-  const std::string filename_;
-};
-
-// Implements random read access in a file using mmap().
-//
-// Instances of this class are thread-safe, as required by the RandomAccessFile
-// API. Instances are immutable and Read() only calls thread-safe library
-// functions.
 class PosixMmapReadableFile final : public RandomAccessFile {
  public:
-  // mmap_base[0, length-1] points to the memory-mapped contents of the file. It
-  // must be the result of a successful call to mmap(). This instances takes
-  // over the ownership of the region.
-  //
-  // |mmap_limiter| must outlive this instance. The caller must have already
-  // aquired the right to use one mmap region, which will be released when this
-  // instance is destroyed.
-  PosixMmapReadableFile(std::string filename, char* mmap_base, size_t length,
-                        Limiter* mmap_limiter)
+  PosixMmapReadableFile(std::string filename, char* mmap_base, size_t length)
       : mmap_base_(mmap_base),
         length_(length),
-        mmap_limiter_(mmap_limiter),
         filename_(std::move(filename)) {}
 
   ~PosixMmapReadableFile() override {
     ::munmap(static_cast<void*>(mmap_base_), length_);
-    mmap_limiter_->Release();
   }
 
   Status Read(uint64_t offset, size_t n, Slice* result,
@@ -237,7 +111,6 @@ class PosixMmapReadableFile final : public RandomAccessFile {
  private:
   char* const mmap_base_;
   const size_t length_;
-  Limiter* const mmap_limiter_;
   const std::string filename_;
 };
 
@@ -277,17 +150,6 @@ class PosixWritableFile final : public WritableFile {
   char* buf_;
   uint32_t size_;
 };
-
-int LockOrUnlock(int fd, bool lock) {
-  errno = 0;
-  struct ::flock file_lock_info;
-  std::memset(&file_lock_info, 0, sizeof(file_lock_info));
-  file_lock_info.l_type = (lock ? F_WRLCK : F_UNLCK);
-  file_lock_info.l_whence = SEEK_SET;
-  file_lock_info.l_start = 0;
-  file_lock_info.l_len = 0;  // Lock/unlock entire file.
-  return ::fcntl(fd, F_SETLK, &file_lock_info);
-}
 
 // Instances are thread-safe because they are immutable.
 class PosixFileLock : public FileLock {
@@ -340,47 +202,14 @@ class PosixEnv : public Env {
 
   Status NewSequentialFile(const std::string& filename,
                            SequentialFile** result) override {
-    int fd = ::open(filename.c_str(), O_RDONLY);
-    if (fd < 0) {
-      *result = nullptr;
-      return PosixError(filename, errno);
-    }
-
-    *result = new PosixSequentialFile(filename, fd);
+    assert(0);
     return Status::OK();
   }
 
   Status NewRandomAccessFile(const std::string& filename,
                              RandomAccessFile** result) override {
-    *result = nullptr;
-    int fd = ::open(filename.c_str(), O_RDONLY);
-    if (fd < 0) {
-      return PosixError(filename, errno);
-    }
-
-    if (!mmap_limiter_.Acquire()) {
-      *result = new PosixRandomAccessFile(filename, fd, &fd_limiter_);
-      return Status::OK();
-    }
-
-    uint64_t file_size;
-    Status status = GetFileSize(filename, &file_size);
-    if (status.ok()) {
-      void* mmap_base =
-          ::mmap(/*addr=*/nullptr, file_size, PROT_READ, MAP_SHARED, fd, 0);
-      if (mmap_base != MAP_FAILED) {
-        *result = new PosixMmapReadableFile(filename,
-                                            reinterpret_cast<char*>(mmap_base),
-                                            file_size, &mmap_limiter_);
-      } else {
-        status = PosixError(filename, errno);
-      }
-    }
-    ::close(fd);
-    if (!status.ok()) {
-      mmap_limiter_.Release();
-    }
-    return status;
+    assert(0);
+    return Status::OK();
   }
 
   Status NewWritableFile(const std::string& filename,
@@ -402,7 +231,8 @@ class PosixEnv : public Env {
   }
 
   bool FileExists(const std::string& filename) override {
-    return ::access(filename.c_str(), F_OK) == 0;
+    assert(0);
+    return true;
   }
 
   Status GetChildren(const std::string& directory_path,
@@ -500,38 +330,13 @@ class PosixEnv : public Env {
       GUARDED_BY(background_work_mutex_);
 
   PosixLockTable locks_;  // Thread-safe.
-  Limiter mmap_limiter_;  // Thread-safe.
-  Limiter fd_limiter_;    // Thread-safe.
 };
-
-// Return the maximum number of concurrent mmaps.
-int MaxMmaps() { return g_mmap_limit; }
-
-// Return the maximum number of read-only files to keep open.
-int MaxOpenFiles() {
-  if (g_open_read_only_file_limit >= 0) {
-    return g_open_read_only_file_limit;
-  }
-  struct ::rlimit rlim;
-  if (::getrlimit(RLIMIT_NOFILE, &rlim)) {
-    // getrlimit failed, fallback to hard-coded default.
-    g_open_read_only_file_limit = 50;
-  } else if (rlim.rlim_cur == RLIM_INFINITY) {
-    g_open_read_only_file_limit = std::numeric_limits<int>::max();
-  } else {
-    // Allow use of 20% of available file descriptors for read-only files.
-    g_open_read_only_file_limit = rlim.rlim_cur / 5;
-  }
-  return g_open_read_only_file_limit;
-}
 
 }  // namespace
 
 PosixEnv::PosixEnv()
     : background_work_cv_(&background_work_mutex_),
-      started_background_thread_(false),
-      mmap_limiter_(MaxMmaps()),
-      fd_limiter_(MaxOpenFiles()) {
+      started_background_thread_(false) {
     g_mem_fd = open("/dev/mem", O_RDWR | O_SYNC);
     if (g_mem_fd == -1) {
       perror("open /dev/mem");
