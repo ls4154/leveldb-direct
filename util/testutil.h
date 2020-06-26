9c183a1085ec27563599ba15090dd9ada80ab4e0
Youngjae Lee <ls4154.lee@gmail.com>
2020-06-26 10:40:23 +0900

    remove memenv

--- util/testutil.h
+++ util/testutil.h
@@ -5,7 +5,6 @@
 #ifndef STORAGE_LEVELDB_UTIL_TESTUTIL_H_
 #define STORAGE_LEVELDB_UTIL_TESTUTIL_H_
 
-#include "helpers/memenv/memenv.h"
 #include "leveldb/env.h"
 #include "leveldb/slice.h"
 #include "util/random.h"
@@ -26,39 +25,6 @@ std::string RandomKey(Random* rnd, int len);
 // the generated data.
 Slice CompressibleString(Random* rnd, double compressed_fraction, size_t len,
                          std::string* dst);
-
-// A wrapper that allows injection of errors.
-class ErrorEnv : public EnvWrapper {
- public:
-  bool writable_file_error_;
-  int num_writable_file_errors_;
-
-  ErrorEnv()
-      : EnvWrapper(NewMemEnv(Env::Default())),
-        writable_file_error_(false),
-        num_writable_file_errors_(0) {}
-  ~ErrorEnv() override { delete target(); }
-
-  Status NewWritableFile(const std::string& fname,
-                         WritableFile** result) override {
-    if (writable_file_error_) {
-      ++num_writable_file_errors_;
-      *result = nullptr;
-      return Status::IOError(fname, "fake error");
-    }
-    return target()->NewWritableFile(fname, result);
-  }
-
-  Status NewAppendableFile(const std::string& fname,
-                           WritableFile** result) override {
-    if (writable_file_error_) {
-      ++num_writable_file_errors_;
-      *result = nullptr;
-      return Status::IOError(fname, "fake error");
-    }
-    return target()->NewAppendableFile(fname, result);
-  }
-};
 
 }  // namespace test
 }  // namespace leveldb
