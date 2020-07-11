667af8fb2f207d3840f73cea33208251a052d6e9
Youngjae Lee <ls4154.lee@gmail.com>
2020-07-11 15:53:35 +0900

    clip file size

--- include/leveldb/options.h
+++ include/leveldb/options.h
@@ -79,7 +79,7 @@ struct LEVELDB_EXPORT Options {
   // so you may wish to adjust this parameter to control memory usage.
   // Also, a larger write buffer will result in a longer recovery time
   // the next time the database is opened.
-  size_t write_buffer_size = 4 * 1024 * 1024;
+  size_t write_buffer_size = 4 * 1000 * 1000;
 
   // Number of open files that can be used by the DB.  You may need to
   // increase this if your database has a large working set (budget
@@ -112,7 +112,7 @@ struct LEVELDB_EXPORT Options {
   // compactions and hence longer latency/performance hiccups.
   // Another reason to increase this parameter might be when you are
   // initially populating a large database.
-  size_t max_file_size = 2 * 1024 * 1024;
+  size_t max_file_size = 4 * 1000 * 1000;
 
   // Compress blocks using the specified compression algorithm.  This
   // parameter can be changed dynamically.
